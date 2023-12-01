8bdb3a7f0683c913b01ab545fe86e9ba2a21d0cc
bokazuya25 <bo.kazuya.25@gmail.com>
2023-12-01 11:25:52 +0900

    購入ページ画面の作成

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -5,30 +5,30 @@
 @endsection
 
 @section('main')
-    <div class="section-wrap">
-        <div class="section-inner__box">
-            <img class="inner__box-image" src="" alt="商品画像">
+    <div class="image-wrap">
+        <div class="image-group">
+            <img class="image-group__image" src="" alt="商品画像">
         </div>
     </div>
 
-    <div class="section-wrap">
-        <div class="section-inner">
-            <h2 class="inner__title inner__title--large">商品名</h2>
-            <p class="inner__text">ブランド名</p>
-            <p class="inner__text inner__text--medium">￥47,000(値段)</p>
-            <div class="inner__unit">
-                <div class="unit__icon">
-                    <img class="unit__icon-image" src="{{ asset('img/star.svg') }}" alt="お気に入り">
+    <div class="detail-wrap">
+        <div class="item-group">
+            <h2 class="item-group__title">商品名</h2>
+            <span class="item-group__brand">ブランド名</span>
+            <p class="item-group__price">￥47,000(値段)</p>
+            <div class="item-unit">
+                <div class="item-icon">
+                    <img class="item-icon__image" src="{{ asset('img/star.svg') }}" alt="お気に入り">
                 </div>
-                <div class="unit__icon">
-                    <img class="unit__icon-image" src="{{ asset('img/comment.svg') }}" alt="コメント">
+                <div class="item__icon">
+                    <img class="item-icon__image" src="{{ asset('img/comment.svg') }}" alt="コメント">
                 </div>
             </div>
             <a class="link-button" href="{{ Auth::user() ? '/purchase' : '/login' }}">購入する</a>
         </div>
-        <div class="section-inner">
-            <h3 class="inner__title">商品説明</h3>
-            <p class="inner__text inner__description-text">
+        <div class="explanation-group">
+            <h3 class="explanation-group__title">商品説明</h3>
+            <p class="explanation-group__text">
                 カラー:グレー<br>
                 <br>
                 新品<br>
@@ -36,16 +36,16 @@
                 <br>
                 購入後、即発送いたします。
         </div>
-        <div class="section-inner">
-            <h3 class="inner__title">商品の情報</h3>
-            <div class="inner__group">
-                <span class="group__title">カテゴリー</span>
-                <span class="group__category">洋服</span>
-                <span class="group__category">メンズ</span>
+        <div class="information-group">
+            <h3 class="information-group__title">商品の情報</h3>
+            <div class="information-content">
+                <span class="information-content__title">カテゴリー</span>
+                <span class="information-content__category">洋服</span>
+                <span class="information-content__category">メンズ</span>
             </div>
-            <div class="inner__group">
-                <span class="group__title">商品の状態</span>
-                <span class="group__text">良好</span>
+            <div class="information-content">
+                <span class="information-content__title">商品の状態</span>
+                <span class="information-content__text">良好</span>
             </div>
         </div>
     </div>
---
+++ src/resources/views/purchase.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
+@endsection
+
+@section('main')
+    <div class="section-wrap">
+        <div class="section-group">
+            <div class="image-content">
+                <img class="image-content__image" src="" alt="商品画像">
+            </div>
+            <div class="item-content">
+                <h2 class="item-content__title">商品名</h2>
+                <p class="item-content__price">￥47,000</p>
+            </div>
+        </div>
+        <div class="payment-group">
+            <div class="header-content">
+                <h3 class="header-content__title">支払方法</h3>
+                <a class="link-button" href="">変更する</a>
+            </div>
+            <p class="group__text">コンビニ払い</p>
+        </div>
+        <div class="address-group">
+            <div class="header-content">
+                <h3 class="header-content__title">配送先</h3>
+                <a class="link-button" href="">変更する</a>
+            </div>
+            <p class="group__text">〇〇県〇〇市△△1丁目-1-1</p>
+        </div>
+    </div>
+
+    <div class="confirm-wrap">
+        <div class="confirm-group">
+            <div class="confirm-content confirm-content__price">
+                <p class="confirm-content__title">商品代金</p>
+                <p class="confirm-content__text">￥47,000</p>
+            </div>
+            <div class="confirm-content confirm-content__total">
+                <p class="confirm-content__title">支払い金額</p>
+                <p class="confirm-content__text">￥47,000</p>
+            </div>
+            <div class="confirm-content confirm-content__payment">
+                <p class="confirm-content__title">支払方法</p>
+                <p class="confirm-content__text">コンビニ払い</p>
+            </div>
+        </div>
+        <button class="submit-button">購入する</button>
+    </div>
+@endsection
